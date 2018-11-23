e39b72a8abdb85a3d52870376f2cb28213afd641
Hannu Huhtanen <hannu.huhtanen@gmail.com>
2018-11-23 21:57:59 +0200

    Testing our dynamoDB integration

---
+++ README.md
@@ -0,0 +1,12 @@
+# Seppo muistaa Telegram bot
+
+A simple telegram bot for creating custom reminders for self. Created as an introduction to serverless technologies.
+
+## Instructions
+
+TODO: Make this part nicer
+
+Commands ran so far:
+* serverless create --template aws-nodejs --path my-service
+* serverless deploy
+* serverless dynamodb install
--- handler.js
+++ handler.js
@@ -1,10 +1,11 @@
 'use strict';
 const rp = require('request-promise');
+const dynamoDb = require('./dynamodb');
 
 const TOKEN = process.env.TELEGRAM_TOKEN;
 const BASE_URL = `https://api.telegram.org/bot${TOKEN}`;
 
-module.exports.hello = async (event, context) => {
+module.exports.hello = async (event, context, callback) => {
 
     try{
         const data = JSON.parse(event.body);
@@ -14,8 +15,56 @@ module.exports.hello = async (event, context) => {
 
         let response = `Please /start, ${firstName}`;
 
-        if (message.includes("start")) {
+        if (message.startsWith("/start")) {
             response = `Hello ${firstName}`;
+        }
+
+        if (message.startsWith("/remember")) {
+            const params = {
+                TableName: process.env.DYNAMODB_TABLE,
+                Item: {
+                    chat_id: chatId,
+                    text: message.split(" ").slice(1).join(" ")
+                }
+            };
+            try {
+                await dynamoDb.put(params).promise();
+                response = `Alright, I'll keep that in mind.`
+            } catch (error) {
+                console.error(error);
+                callback(null, {
+                    statusCode: error.statusCode || 501,
+                    headers: { 'Content-Type': 'text/plain' },
+                    body: 'Couldn\'t create the todo item.'
+                });
+            }
+        }
+
+        if (message.startsWith("/recall")) {
+            const params = {
+                TableName: process.env.DYNAMODB_TABLE,
+                Key: {
+                    chat_id: chatId
+                },
+                AttributesToGet: [
+                    'text'
+                ]
+            };
+            try {
+                const data = await dynamoDb.get(params).promise();
+                if (data.Item) {
+                    response = `You asked me to remember "${data.Item.text}"`;
+                } else {
+                    response = "Sorry I don't remember anything. Can you remind me with /remember";
+                }
+            } catch (error) {
+                console.error(error);
+                callback(null, {
+                    statusCode: error.statusCode || 501,
+                    headers: { 'Content-Type': 'text/plain' },
+                    body: 'Couldn\'t create the todo item.'
+                });
+            }
         }
 
         const responseData = { text: response, chat_id: chatId };
